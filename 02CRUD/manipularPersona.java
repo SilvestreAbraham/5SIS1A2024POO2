27344fe27378cb3a50d4c45608c3a757253e1e0b
SilvestreAbraham <137237169+SilvestreAbraham@users.noreply.github.com>
2024-01-22 10:02:09 -0600

    Add files via upload

---
+++ 02CRUD/manipularPersona.java
@@ -0,0 +1,21 @@
+
+
+import java.util.*;
+
+public class manipularPersona{
+
+    public void menu(){
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Bienvenido a este Programa");
+        System.out.println("Elija una Opcion Valida");
+        System.out.println("1.- Mostrar todos los registros de personas");
+        System.out.println("2.- Registrar un nuevo objeto");
+        System.out.println("3.- Editar ese Objeto");
+        System.out.println("4.- Borrar el Objeto");
+        System.out.println("5.- Salir");
+        
+
+    }
+}
