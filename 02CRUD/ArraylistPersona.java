a4b9fbd9176439ad464e4050888cb4b7f491e341
SilvestreAbraham <137237169+SilvestreAbraham@users.noreply.github.com>
2024-01-23 22:49:14 -0600

    ArraylistPersona.java

---
+++ 02CRUD/ArraylistPersona.java
@@ -0,0 +1,12 @@
+
+package ArrarylistPersona;
+
+
+public class Arrarylistersona {
+
+    public static void main(String[] args) {
+       
+        vistaManipular inter = new vistaManipular();
+        inter.Principal();
+    }
+    
